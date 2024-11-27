422a4eac0ee6e4b148981167acb3d73bda60d7bc
matthewjosephbilaos-cp <matthewjoseph.bilaos@coreproc.com>
2024-11-27 17:14:17 +0800

    Edit: Uncomment User Factory, Add ProductSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Product;
 use Illuminate\Database\Seeder;
+use Database\Seeders\ProductSeeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,9 +18,14 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        if (Product::count() === 0) {
+            $this->call(ProductSeeder::class);
+        }
     }
 }
