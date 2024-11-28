e4a26ebf2102f30d03ca5ad111b705feca0b5cdf
matthewjosephbilaos-cp <matthewjoseph.bilaos@coreproc.com>
2024-11-28 08:33:46 +0800

    Edit: Add Foreign Id category_id and brand_id

--- database/migrations/2024_11_27_060923_create_products_table.php
+++ database/migrations/2024_11_27_060923_create_products_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnUpdate()->cascadeOnDelete();
+            $table->foreignId('brand_id')->constrained('brands')->cascadeOnUpdate()->cascadeOnDelete();
             $table->string('title', 200);
             $table->longText('description')->nullable();
             $table->integer('quantity');
