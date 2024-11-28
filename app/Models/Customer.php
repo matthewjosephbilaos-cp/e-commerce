7ff8cbc0f764e4710e8a935fe3b982cd105921ba
matthewjosephbilaos-cp <matthewjoseph.bilaos@coreproc.com>
2024-11-28 13:23:35 +0800

    Add: Customer Model

---
+++ app/Models/Customer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $table = 'customers';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    public function productsOrdered() : BelongsToMany {
+        return $this->belongsToMany(Product::class);
+    }
+}
